5d927839c2b330e10862227c73c6c33dd5bd09c4
jennifer hodge <jenniferhodge@jennifers-Air.lan>
2024-03-29 19:44:47 -0500

    so far so good

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./Header.css";
 import Header from "./Header.jsx";
+import CharacterRatings from "./Character-ratings.jsx";
+import "./Character-rating.css";
 
 function App() {
   return (
     <>
       <Header />
+      <CharacterRatings />
     </>
   );
 }
---
+++ src/Character-ratings.jsx
@@ -0,0 +1,9 @@
+export function CharacterRatings() {
+  return (
+    <section id="character-ratings">
+      <h4>Top Characters</h4>
+    </section>
+  );
+}
+
+export default CharacterRatings;
